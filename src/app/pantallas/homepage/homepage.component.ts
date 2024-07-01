b7210bcc1257ffe2dd4c922db9104c3bc0952d21
Nacho <nachoman2030@gmail.com>
2024-07-01 16:30:04 -0300

    correcion de errores de fondo de pantalla y lista

--- src/app/pantallas/homepage/homepage.component.ts
+++ src/app/pantallas/homepage/homepage.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrls: ['./homepage.component.css'] // Corregir a styleUrls en plural
 })
-export class HomepageComponent{
-  
-}
+export class HomepageComponent {}
